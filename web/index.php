d59918dd8adf9f8b537b48db1fdecd5fc1eb6832
Josh Weigel <jkd1703@wmich.edu>
2018-11-28 16:09:18 -0500

    starting the switch from twig to regular html

--- web/index.php
+++ web/index.php
@@ -2,24 +2,58 @@
 
 require('../vendor/autoload.php');
 
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
+
 // Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+$app->register(
+  new Silex\Provider\MonologServiceProvider(),
+  array(
+    'monolog.logfile' => 'php://stderr',
+  )
+);
 
 // Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
+/*/
+$app->register(
+  new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
-));
+  )
+);
+//*/
+
+
 
 // Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+$app->get(
+  '/',
+  function() use($app) {
+    $app['monolog']->addDebug( 'logging output.' );
+
+    return $app->sendFile(
+      './client/index.html'
+    );
+  }
+);
+
+$app->post(
+  '/test-post/',
+  function() use($app) {
+    $app['monolog']->addDebug( 'logging output.' );
+
+
+    $responseData = array(
+      'test1' => 'this is a test',
+      'test2' => 'to see how silax does post endpoints'
+    );
+
+    return $app->json(
+      $responseData
+    );
+  }
+);
 
 $app->run();
